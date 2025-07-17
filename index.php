c009135fd31811046327b6ef4ced68382a5db431
JutzuTai <maristelajeferson14@gmail.com>
2025-07-17 10:35:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    echo "My name is: Jeferson P. Maristela <br>";
+    echo "I am 21 years old <br>";
+    echo "My favorite subject is: Understanding Self <br>";
+    echo "My dream job is: Business Man <br>";
+    echo "My motto in life: Work Hard, Fly High Butterfly <br><br>";
+    echo "Here is my ASCII Art: <br><pre>";
+
+    echo <<<ASCII
+                     ;'-. 
+        `;-._        )   '---.._
+          >  `-.__.-'           `'.__
+        /_.-'-._         _,,    ^ ---)
+        `       `'------/_.'-----```` 
+                        `
+    ASCII
+?>

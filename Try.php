d53d8b61a71d08c01ef4a509a02fb53b60e80b84
i'm hot maria clara <maristelajeferson14@gmail.com>
2025-08-14 12:07:33 +0800

    Add files via upload

---
+++ Try.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-4"> <!-- Small width -->
+        <div class="card shadow-sm rounded-4">
+          <div class="card-header bg-primary text-white text-center">
+            <h5>Enter Two Numbers</h5>
+          </div>
+          <div class="card-body">
+            <form method="$_GET" action="">
+              <div class="mb-3">
+                <label for="firstNumber" class="form-label">First Number</label>
+                <input type="number" class="form-control" id="firstNumber" placeholder="First number" name="num1">
+              </div>
+
+              <div class="mb-3">
+                <label for="secondNumber" class="form-label">Second Number</label>
+                <input type="number" class="form-control" id="secondNumber" placeholder="Second number" name="num2">
+              </div>
+
+              <button type="submit" class="btn btn-success w-100">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+<?php
+    // echo $_GET['num1'];
+    // echo $_GET['num2'];
+    // $sum = $_GET['num1']; + $_GET['num2'];
+
+    $x = $_GET['num1'];
+    $y = $_GET['num2'];
+
+    $sum = $x + $y;
+
+    echo "<br>The sum is : $sum";
+?>

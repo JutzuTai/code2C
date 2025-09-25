385db886f78cb9891c1dd9edc39998e0da64634c
i'm hot maria clara <maristelajeferson14@gmail.com>
2025-09-25 12:19:40 +0800

    Add files via upload

---
+++ db/db_connect.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = "localhost"; // Server host
+
+$user = "root"; // MySQL username
+
+$pass = ""; // MySQL password (leave blank if none)
+
+$db = "students"; // Database name
+
+ 
+
+// Create connection
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+
+if ($conn->connect_error) {
+
+die("Connection failed: " . $conn->connect_error);
+
+}
+
+?>
